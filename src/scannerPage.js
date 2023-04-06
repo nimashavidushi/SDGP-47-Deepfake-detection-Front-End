dcad2ce55ef2486d80163a3eb983e091d4cd558c
chandeepag <chandeepa.gunaratne@gmail.com>
2023-04-06 12:25:35 +0530

    final-changes-fe

--- src/scannerPage.js
+++ src/scannerPage.js
@@ -2,32 +2,20 @@ import React, { useState } from "react";
 import "./scannerPage.css";
 import { Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import FileUploader from './FileUploader';
 
 function useScannerPg() {
   const [videoFile, setVideoFile] = useState(null);
 
-  const handleFileUpload = (e) => {
-    const file = e.target.files[0];
+  const handleFileUpload = (file) => {
     setVideoFile(file);
   };
 
-  const handleUpload = () => {
+/*   const handleUpload = () => {
     if (videoFile) {
-      const formData = new FormData();
-      formData.append("video", videoFile);
-
-      fetch("/upload-video", {
-        method: "POST",
-        body: formData,
-      })
-        .then((response) => {
-          console.log(response);
-        })
-        .catch((error) => {
-          console.error(error);
-        });
+      // upload video file to server
     }
-  };
+  }; */
 
   return (
     <div className="container">
@@ -60,12 +48,8 @@ function useScannerPg() {
       <main className="row">
         <div className="col">
           <div className="dropzone">
-            <form className="form-inline">
-              <div className="form-group">
-                <input type="file" className="form-control-file" onChange={handleFileUpload} />
-              </div>
-              <button type="button" className="btn btn-primary" onClick={handleUpload}>Scan</button>
-            </form>
+            <FileUploader onFileUpload={handleFileUpload} />
+           
           </div>
         </div>
       </main>
